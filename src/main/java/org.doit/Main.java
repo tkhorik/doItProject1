ffb754ca2a43a14ad7e29aa11cef3e2956e124d1
marv's Mac <marvelmd@gmail.com>
2019-10-10 00:13:21 +0300

    component scan вынесен за пределы класса в xml file

--- src/main/java/org.doit/Main.java
+++ src/main/java/org.doit/Main.java
@@ -3,13 +3,14 @@ package org.doit;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         MessageRenderer renderer = context.getBean("simpleMessageRenderer",MessageRenderer.class);
         renderer.render();
 
