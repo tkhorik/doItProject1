fdf7840341318af28fcde309dabe78b924cc625a
marv's Mac <marvelmd@gmail.com>
2019-10-09 23:29:46 +0300

    spring context realization

--- src/main/java/org.doit/Main.java
+++ src/main/java/org.doit/Main.java
@@ -1,16 +1,21 @@
 package org.doit;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
-
-
-        MessageProvider provider = MessageFactory.getProvider();
-
-
-        MessageRenderer renderer = MessageFactory.getRenderer();
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        MessageRenderer renderer = context.getBean("renderer",MessageRenderer.class);
         renderer.render();
+
+
+       /* MessageProvider provider = MessageFactory.getProvider();
+        MessageRenderer renderer = MessageFactory.getRenderer();
+        renderer.render();*/
 
     }
 }
